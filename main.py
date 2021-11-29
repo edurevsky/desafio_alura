d89ff9022eb6a052ec763fce28d183d4ad0a014d
Eduardo Stocchero de Oliveira <91025690+edurevsky@users.noreply.github.com>
2021-11-29 15:18:06 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -30,6 +30,77 @@ def pedeID():
         return None
 
 
+def editarNomeAluno(index):
+    nome = input('Digite o nome a ser mudado: ')
+    esc.mudarNomeDoAluno(index, nome)
+    print(f'Aluno (ID: {index}) teve nome alterado para {nome}.')
+
+
+def editarEmailAluno(index):
+    email = pedeEmail()
+    esc.mudarEmailDoAluno(index, email)
+    print(f'Email do aluno (ID: {index}) mudado para {email}.')
+
+
+def editarTurmaAluno(index):
+    turma = input('Digite a nova turma do aluno: ')
+    esc.mudarTurmaDoAluno(index, turma)
+    print(f'Turma do aluno (ID: {index}) mudado para {turma}')
+
+
+def editarTurnoAluno(index):
+    print('Para qual turno?\n[1] - Manha | [2] - Tarde | [3] - Noite')
+    comando = int(input('Digite o comando >> '))
+    if comando == 1:
+        esc.mudarTurnoDoAlunoParaManha(index)
+        print(f'Turno do aluno (ID: {index}) mudado para Manha')
+    elif comando == 2:
+        esc.mudarTurnoDoAlunoParaTarde(index)
+        print(f'Turno do aluno (ID: {index}) mudado para Tarde')
+    elif comando == 3:
+        esc.mudarTurnoDoAlunoParaNoite(index)
+        print(f'Turno do aluno (ID: {index}) mudado para Noite')
+
+
+def editarStatusAluno(index):
+    print('Mudar para:\n[1] - Ativo | [2] - Inativo')
+    comando = int(input('Digite o comando >> '))
+    if comando == 1:
+        esc.mudarStatusAlunoParaAtivo(index)
+        print(f'Status do aluno (ID: {index}) mudado para Ativo')
+    elif comando == 2:
+        esc.mudarStatusAlunoParaInativo(index)
+        print(f'Status do aluno (ID: {index}) mudado para Inativo')
+
+
+def mostraDadosAlunoComOpcoes(index):
+    if index != None:
+        esc.listarAlunoComIndex(index)
+    comando = int(input('Deseja editar o aluno? [1] - Sim | [0] - Não\nDigite o comando >> '))
+    if comando == 1:
+        print('Você quer editar:\n[1] - Nome | [2] - Email | [3] - Turma | [4] - Turno | [5] - Status')
+        comando = int(input('Digite o comando >> '))
+        # Muda nome do aluno
+        if comando == 1:
+            editarNomeAluno(index)
+        # Muda email do aluno
+        elif comando == 2:
+            editarEmailAluno(index)
+        # Muda turma do aluno
+        elif comando == 3:
+            editarTurmaAluno(index)
+        # Muda Turno do aluno
+        elif comando == 4:
+            editarTurnoAluno(index)
+        # Muda Status do aluno
+        elif comando == 5:
+            editarStatusAluno(index)
+        else:
+            print('Opção não existe, voltando ao menu principal')
+    else:
+        pass # Não fazer nada
+
+
 if __name__ == '__main__':
 
     esc = Escola()
@@ -61,54 +132,7 @@ if __name__ == '__main__':
 
         elif comando == 5:
             index = pedeID()
-            if index != None:
-                esc.listarAlunoComIndex(index)
-            comando = int(input('Deseja editar o aluno? [1] - Sim | [0] - Não\nDigite o comando >> '))
-            if comando == 1:
-                print('Você quer editar:\n[1] - Nome | [2] - Email | [3] - Turma | [4] - Turno | [5] - Status')
-                comando = int(input('Digite o comando >> '))
-                # Muda nome do aluno
-                if comando == 1:
-                    nome = input('Digite o nome a ser mudado: ')
-                    esc.mudarNomeDoAluno(index, nome)
-                    print(f'Aluno (ID: {index}) teve nome alterado para {nome}.')
-                # Muda email do aluno
-                elif comando == 2:
-                    email = pedeEmail()
-                    esc.mudarEmailDoAluno(index, email)
-                    print(f'Email do aluno (ID: {index}) mudado para {email}.')
-                # Muda turma do aluno
-                elif comando == 3:
-                    turma = input('Digite a nova turma do aluno: ')
-                    esc.mudarTurmaDoAluno(index, turma)
-                    print(f'Turma do aluno (ID: {index}) mudado para {turma}')
-                # Muda Turno do aluno
-                elif comando == 4:
-                    print('Para qual turno?\n[1] - Manha | [2] - Tarde | [3] - Noite')
-                    comando = int(input('Digite o comando >> '))
-                    if comando == 1:
-                        esc.mudarTurnoDoAlunoParaManha(index)
-                        print(f'Turno do aluno (ID: {index}) mudado para Manha')
-                    elif comando == 2:
-                        esc.mudarTurnoDoAlunoParaTarde(index)
-                        print(f'Turno do aluno (ID: {index}) mudado para Tarde')
-                    elif comando == 3:
-                        esc.mudarTurnoDoAlunoParaNoite(index)
-                        print(f'Turno do aluno (ID: {index}) mudado para Noite')
-                # Muda Status do aluno
-                elif comando == 5:
-                    print('Mudar para:\n[1] - Ativo | [2] - Inativo')
-                    comando = int(input('Digite o comando >> '))
-                    if comando == 1:
-                        esc.mudarStatusAlunoParaAtivo(index)
-                        print(f'Status do aluno (ID: {index}) mudado para Ativo')
-                    elif comando == 2:
-                        esc.mudarStatusAlunoParaInativo(index)
-                        print(f'Status do aluno (ID: {index}) mudado para Inativo')
-                else:
-                    print('Opção não existe, voltando ao menu principal')
-            else:
-                pass # Não fazer nada
+            mostraDadosAlunoComOpcoes(index)
 
         elif comando == 6:
             index = pedeID()
